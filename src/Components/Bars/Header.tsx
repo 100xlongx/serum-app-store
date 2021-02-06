943b86ff0fe6c50c3c23d029a97b7029be64af69
raaedk <raaedk@gmail.com>
2021-02-05 20:11:44 -0500

    routing reimplemented

--- src/Components/Bars/Header.tsx
+++ src/Components/Bars/Header.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import { Pane, Image, Link } from 'evergreen-ui';
 
+import { Link as RouterLink } from 'react-router-dom';
+
 import logo from "../../assets/images/serum192.png";
 
 import SocialMediaIcons from "../Socials/SocialMediaIcons";
@@ -12,11 +14,12 @@ const Header: React.FC = () => {
             <Pane flex={1} alignItems="center" display="flex">
                 <Image marginRight='24px' src={logo} width={48}/>
 
-                <Pane display='flex' justifyContent='center' alignItems='center' borderRadius={4} padding='6px' marginRight='16px'><Link textDecoration='none' color='green' href="/">Applications</Link></Pane>
-                <Pane display='flex' justifyContent='center' borderRadius={4} padding='6px' marginRight='16px'><Link textDecoration='none' color='green' href="/medium">Medium</Link></Pane>
+                {/* {textDecoration='none' color='green' href="/"} */}
+
+                <Pane display='flex' justifyContent='center' alignItems='center' borderRadius={8} padding='8px' marginRight='16px'><RouterLink className='linkbox' to='/'>Applications</RouterLink></Pane>
+                <Pane display='flex' justifyContent='center' borderRadius={8} padding='8px' marginRight='16px'><RouterLink className='linkbox' to='/medium'>Medium</RouterLink></Pane>
             </Pane>
             <Pane>
-                {/* {Buttons.map((button, index) => <Button key={index} marginRight={16} onClick={() => {history.push(`/${button.urlSuffix}`)}} >{button.buttonText}</Button>)} */}
                 <SocialMediaIcons/>
             </Pane>
         </Pane>
